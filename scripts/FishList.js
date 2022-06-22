30eab80a99318eeb994cbda9c0dcaec0b28d8f4f
Cara Netzer <cara.netzer@gmail.com>
2022-06-22 09:58:06 -0400

    fish as objects version

---
+++ scripts/FishList.js
@@ -0,0 +1,24 @@
+// Import the function that returns a copy of the fish array
+import { getFish } from "./database.js"
+
+export const FishList = () => {
+    // Invoke the function that you imported from the database module
+    const fishes = getFish();
+
+    // Start building a string filled with HTML syntax
+    let htmlString = '<h2>Fish in My Collection</h2>'
+
+    // Create HTML representations of each fish here
+    for (const fish of fishes) {
+        htmlString += `<section class="fish">
+            <div class="fish-image"><img  class="pic" src="${fish.image}" ' alt='Tuna from Stardew Valley'/></div>
+            <div><div class="fish__name"><b>Name</b>: ${fish.name}\n</div>
+            <div class="fish__species"><b>Species</b>: ${fish.species}\n</div>
+            <div class="fish__length"><b>Length</b>: ${fish.length} inches\n</div>
+            <div class="fish__location"><b>Found In</b>: ${fish.origin}\n</div>
+            <div class="fish__diet"><b>Diet</b>: ${fish.food}\n</div></div>
+        </section>`;
+    }
+
+    return htmlString;
+}
